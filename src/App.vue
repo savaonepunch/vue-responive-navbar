ccc8ed726ed095c8dff04ff964c42c3ed34ee141
Sava <87912493+savaonepunch@users.noreply.github.com>
2022-06-15 02:10:16 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <Header />
+  <div class="content">
+    <div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div>
+    <div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div><div class="card">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        mollitia maiores voluptate corporis atque porro quia autem, facilis
+        totam explicabo labore modi consectetur tenetur voluptatem cumque itaque
+        quibusdam quae consequatur?
+      </p>
+      <img src="./assets/images/frog.jpg" alt="">
+    </div>
+  </div>
+</template>
+
+<style>
+@import "./assets/styles/main.css";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="headerDesktop">
+    <div class="logo">
+      <a v-if="!darkMode" href="#"><img src="../assets/images/LogoDark.png" alt="Logo" /></a>
+      <a v-else href="#"><img src="../assets/images/Logo.png" alt="Logo" /></a>
+    </div>
+    <div class="navList">
+      <div class="navItem"><a href="#">Home</a></div>
+      <div class="navItem"><a href="#">Docs</a></div>
+      <div class="navItem"><a href="#">API</a></div>
+      <div class="navItem"><a href="#">Examples</a></div>
+      <div class="navItem"><a href="#">Community</a></div>
+      <div @click="switchTheme" class="navItem">
+        <i v-if="!darkMode" class="fa-solid fa-sun fa-xl"></i>
+        <i v-else class="fa-solid fa-moon fa-xl"></i>
+      </div>
+    </div>
+  </div>
+  <Transition>
+    <div v-if="!menuOpen" class="headerMobile">
+      <div @click="openMenu" class="menuBtn">
+        <img v-if="!darkMode" src="../assets/images/menubtndark.png" alt="" />
+        <img v-else src="../assets/images/menubtn.png" alt="" />
+      </div>
+      <div class="logo">
+        <a v-if="!darkMode" href="#"><img src="../assets/images/LogoDark.png" alt="Logo" /></a>
+        <a v-else href="#"><img src="../assets/images/Logo.png" alt="Logo" /></a>
+      </div>
+    </div>
+    <div v-else class="headerMobileOpen">
+      <div @click="closeMenu" class="menuBtn">
+        <img v-if="!darkMode" src="../assets/images/closebtndark.png" alt="" />
+        <img v-else src="../assets/images/closebtn.png" alt="" />
+      </div>
+      <div class="logo">
+        <a v-if="!darkMode" href="#"><img src="../assets/images/LogoDark.png" alt="Logo" /></a>
+        <a v-else href="#"><img src="../assets/images/Logo.png" alt="Logo" /></a>
+      </div>
+      <div @click="switchTheme" class="themeBtn">
+        <i v-if="!darkMode" class="fa-solid fa-sun fa-2xl"></i>
+        <i v-else class="fa-solid fa-moon fa-2xl"></i>
+      </div>
+
+      <div class="navList">
+        <div class="navItem"><a href="#">Home</a></div>
+        <div class="navItem"><a href="#">Docs</a></div>
+        <div class="navItem"><a href="#">API</a></div>
+        <div class="navItem"><a href="#">Examples</a></div>
+        <div class="navItem"><a href="#">Community</a></div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import { ref, watch, onMounted } from "vue";
+
+const menuOpen = ref(false);
+const darkMode = ref(true);
+
+const openMenu = () => {
+  menuOpen.value = true;
+};
+
+const closeMenu = () => {
+  menuOpen.value = false;
+};
+
+const switchTheme = () => {
+  darkMode.value = !darkMode.value;
+};
+
+watch(menuOpen, () => {
+  if (menuOpen.value) {
+    console.log("menu is open");
+  }
+});
+
+watch(darkMode, () => {
+  if (darkMode.value) {
+    document.body.classList.remove('light-mode')
+    document.body.classList.add('dark-mode')
+  }
+  else {
+    document.body.classList.remove('dark-mode')
+    document.body.classList.add('light-mode')
+  }
+});
+
+onMounted(() => {
+  document.body.classList.add('dark-mode')
+})
+
+</script>
